3963b543ae837af2a0e86cb5276300cbd4dc8022
crazyblue7 <mewocoolguy@hi.com>
2026-08-13 19:31:54 -0300

    Increase font size
    Increased font size cuz i dont use a big monitor and my laptop screen
    stays at a big-ish distance (maybe at most a meter?) and the previous
    font size was quite small at the distance from the laptop screen

--- script.js
+++ script.js
@@ -3,30 +3,12 @@ let quaso = "quaso";
 function docID(abajab) {
     return document.getElementById(abajab);
 }
-let bodyportfolio = docID("portfolio");
-let body = docID("mainbody");
-if (body != null) {
-    setInterval(() => {
-        let separatorSpace = docID("separatorSpace").value;
-        console.log(separatorSpace);
-        // console.log(quaso);
-        let separator1 = docID("separator1");
-        separator1.style.fontSize =  String(separatorSpace) + "px";
-        let separator2 = docID("separator2");
-        separator2.style.fontSize =  String(separatorSpace) + "px";
-    }, 30);
-}
-if (bodyportfolio != null) {
-    
-    let finishedEle = docID("finished");
-    let good = docID("goodMorning")
-    
-    let finished = false;
-    setInterval(() => {
-        finishedEle.hidden = finished;
-        let minimumSize = 20;
-        if (Number(good.style.fontSize-"px") < minimumSize) {
-            good.style.fontSize = minimumSize + "px";
-        } 
-    }, 30);    
-}
+setInterval(() => {
+	let separatorSpace = docID("separatorSpace").value;
+	// console.log(separatorSpace);
+	// console.log(quaso);
+	let separator1 = docID("separator1");
+	separator1.style.fontSize =  String(separatorSpace) + "px";
+	let separator2 = docID("separator2");
+	separator2.style.fontSize =  String(separatorSpace) + "px";
+}, 30);
