0486adab0a82459d7d1d42580035e12945215598
crazyblue7 <mewocoolguy@hi.com>
2026-09-23 18:44:03 -0300

    touch grass dude

---
+++ touch-grass-dude/script.js
@@ -0,0 +1,25 @@
+let mx = 0;
+let my = 0;
+let mmx = 0;
+let mmy = 0;
+
+let hand = document.getElementById("hand");
+
+function getmousepos(mev) {
+	mx = mev.clientX;
+	my = mev.clientY;
+	mmx = mev.movementX;
+	mmy = mev.movementY;
+}
+function x(num,x) {if (num > x) {return num-x;}else {return num-x;}}
+
+function uh(event) {
+	getmousepos(event);
+	hand.style.left = mx + "px";
+	hand.style.top  = (my- 140) + "px";
+	hand.style.rotate     = (-45+mmx/2-mmy/2) + "deg";
+	// image is 1540x4096
+}
+hand.style.position = "fixed";
+hand.style.rotate   = (-45+mmx/2) + "deg";
+document.getElementById("hi").addEventListener("mousemove", uh);
